735ab9e53a4ab11dc0f349f5f0e9bb280e99dd2a
Michael Rice <michael@riceclan.org>
2023-04-22 22:46:57 -0500

    working prototype

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-// #include <string>
+#include <string>
 #include <vector>
-// #include <filesystem>
+#include <filesystem>
 #include <unistd.h>
-// #include <sstream>
-// #include <iterator>
+#include <sstream>
+#include <iterator>
 #include <fstream>
 
 namespace fs = std::__fs::filesystem;
@@ -19,14 +19,32 @@ static bool startsWith(const std::string& str, const std::string& prefix)
     return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
 }
 
+std::string readWrapperConfig(fs::path path) {
+    // fs::path path(path_arg);
+    std::cout << "Found path = " << path.string() << std::endl;
+    std::ifstream file(path.string());
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.find("PATH=") == 0) {
+            size_t start_pos = line.find_first_of("=") + 1;
+            size_t end_pos = line.find_first_of("#", start_pos);
+            // Remove any leading spaces before the comment
+            while (std::isspace(line[end_pos-1])) {
+                --end_pos;
+            }
+            std::string found_path = line.substr(start_pos, end_pos-start_pos);
+            return found_path;
+        }
+    }
+    return "";
+}
+
 int main(int argc, char* argv[]) {
     // Get the name of the program being called, without any path elements
     std::string program_name = fs::path(argv[0]).filename().string();
-    std::cerr << "Using program_name=" << program_name.c_str() << std::endl;
 
     // Get the path of the current program, and replace "bin" with "config"
     fs::path program_path = fs::absolute(argv[0]).parent_path();
-    std::cerr << "Before searching for '.', program_path = " << program_path << std::endl;
 
     // strip any trailing '.'
     if (endsWith(program_path, ".")) {
@@ -47,11 +65,11 @@ int main(int argc, char* argv[]) {
     // Split the WRAPPER_PATH on ':'
     // add the elements to paths if they are absolute directories that exist
     std::vector<fs::path> paths;
-    std::string path_str(wrapper_path);
+    std::string wrapper_str(wrapper_path);
     size_t start = 0;
-    size_t end = path_str.find(':');
+    size_t end = wrapper_str.find(':');
     while (end != std::string::npos) {
-        fs::path dir_path(path_str.substr(start, end - start));
+        fs::path dir_path(wrapper_str.substr(start, end - start));
         if (dir_path.is_absolute() && fs::exists(dir_path) && fs::is_directory(dir_path)) {
             // std::cerr << dir_path.string() << " is acceptable" << std::endl;
             paths.push_back(dir_path);
@@ -59,9 +77,9 @@ int main(int argc, char* argv[]) {
             // std::cerr << dir_path.string() << " is not absolute, a directory, or existing... skipping" << std::endl;
         }
         start = end + 1;
-        end = path_str.find(':', start);
+        end = wrapper_str.find(':', start);
     }
-    fs::path dir_path(path_str.substr(start));
+    fs::path dir_path(wrapper_str.substr(start));
     if (dir_path.is_absolute() && fs::exists(dir_path) && fs::is_directory(dir_path)) {
         // std::cerr << dir_path.string() << " is acceptable" << std::endl;
         paths.push_back(dir_path);
@@ -72,37 +90,34 @@ int main(int argc, char* argv[]) {
     paths.push_back(program_path);
 
     // Print out the list of paths
-    std::cout << "Search Directories" << std::endl;
-    for (const auto& path : paths) {
-        std::cout << "  " << fs::path(path).string() << std::endl;
-    }
+    // std::cout << "Search Directories" << std::endl;
+    // for (const auto& path : paths) {
+    //     std::cout << "  " << fs::path(path).string() << std::endl;
+    // }
 
-    // Look for the program_name in the directories
+    // Look through the directories, if you find program_name, read it for
+    // PATH=
     for (const auto& dir : paths) {
         std::cout << "checking " << dir << " for " <<  program_name << std::endl;
         fs::path path = dir / program_name;
         if (fs::exists(path) && fs::is_regular_file(path)) {
+            std::string new_dir = readWrapperConfig(path.string());
+            fs::path new_path(new_dir);
+            new_path = new_path / program_name;
             // Found the program config, read the PATH value from the file
-            std::ifstream file(path.string());
-            std::string line;
-            while (std::getline(file, line)) {
-                if (line.find("PATH=") == 0) {
-                    // Modify the PATH environment variable and execute the program
-                    std::string new_path = line.substr(5);
-                    std::string old_path = std::getenv("PATH");
-                    std::string merged_path = new_path + ":" + old_path;
-                    setenv("PATH", merged_path.c_str(), 1);
-                    std::cout << "path is " << path << std::endl;
-                    execv(path.c_str(), argv);
-                    perror("execv");
-                    return 1;
-                }
+            std::string old_path = std::getenv("PATH");
+            if (old_path.empty()) {
+                std::cerr << "PATH environment variable not found, aborting" << std::endl;
+                return 1;
             }
-            std::cerr << "Could not find PATH= line in " << path << std::endl;
+            std::string merged_path = new_path.string() + ":" + old_path;
+            setenv("PATH", merged_path.c_str(), 1);
+            std::cout << "path is " << path << std::endl;
+            execv(new_path.c_str(), argv);
+            perror("execv");
             return 1;
         }
     }
-
     // std::cerr << "Could not find executable " << program_name << " in WRAPPER_PATH and program path." << std::endl;
     // return 1;
 }
